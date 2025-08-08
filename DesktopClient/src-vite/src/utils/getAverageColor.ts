bef6391ad0389431111237ce01ba8c432decf645
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-08-08 22:50:56 +0300

    replace fast-average-color with node-vibrant

--- DesktopClient/src-vite/src/utils/getAverageColor.ts
+++ DesktopClient/src-vite/src/utils/getAverageColor.ts
@@ -1,22 +1,7 @@
-import { FastAverageColor } from 'fast-average-color'
+import { Vibrant } from "node-vibrant/browser";
 
 export async function getAverageColor(img: HTMLImageElement | null) {
-  const fac = new FastAverageColor()
-
-  return await fac.getColorAsync(img, {
-    mode: 'precision',
-    algorithm: 'dominant',
-    ignoredColor: [
-      [255, 255, 255, 255, 90], // White
-      [0, 0, 0, 255, 70], // Black
-      [0, 0, 0, 0, 40], // Transparent
-      [255, 255, 50, 255, 70], // Bright Yellow
-      [100, 255, 10, 255, 50], // Bright Green
-      [200, 20, 200, 255, 30], // Vibrant Purple
-    ],
-    // if the image only contains ignored colors, returns the primary color instead.
-    defaultColor: [16, 183, 127, 1],
-  })
+  return img.src ? (await Vibrant.from(img!.src).getPalette()).Vibrant : "";
 }
 
 export function hexToRgba(hex: string, alpha: number = 1) {
