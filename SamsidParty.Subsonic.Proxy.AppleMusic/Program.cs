afb68358d1ac8f57e23bb46dc21b2b7f576410b6
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-08-10 11:58:35 +0300

    Refactor: No need for AppleMusicProxyController subclass

--- SamsidParty.Subsonic.Proxy.AppleMusic/Program.cs
+++ SamsidParty.Subsonic.Proxy.AppleMusic/Program.cs
@@ -8,19 +8,8 @@ Keys.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddSingleton<AppleMusicRequestImplementation, AppleMusicRequestImplementation>();
+builder.Services.AddSingleton<IController, AppleMusicRequestImplementation>();
 builder.Services.AddMvc()
-    .ConfigureApplicationPartManager((ApplicationPartManager apm) =>
-    {
-        // Prevents the base class controller from being mapped
-        var commonLib = apm.ApplicationParts.FirstOrDefault(a => a.Name == "SamsidParty.Subsonic.Common");
-
-        if (commonLib != null)
-        {
-            apm.ApplicationParts.Remove(commonLib);
-        }
-    })
     .AddNewtonsoftJson();
 
 var app = builder.Build();
