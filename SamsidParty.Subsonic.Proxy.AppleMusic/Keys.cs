99ae0007b79efaee6790ee9def2962555ad1832e
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-08-10 03:51:15 +0300

    Implement env loading

---
+++ SamsidParty.Subsonic.Proxy.AppleMusic/Keys.cs
@@ -0,0 +1,14 @@
+﻿namespace SamsidParty.Subsonic.Proxy.AppleMusic
+{
+    public class Keys
+    {
+        public static string? AppleDeveloperToken;
+        public static string? MusicUserToken;
+
+        public static void Load()
+        {
+            AppleDeveloperToken = Environment.GetEnvironmentVariable("APPLE_DEVELOPER_TOKEN");
+            MusicUserToken = Environment.GetEnvironmentVariable("MUSIC_USER_TOKEN");
+        }
+    }
+}
