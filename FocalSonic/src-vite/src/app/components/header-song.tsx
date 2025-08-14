cc65f68441ff5523a380c2424fb65108484a9492
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-08-14 08:42:42 +0300

    Tweak: Adjust title in header

--- FocalSonic/src-vite/src/app/components/header-song.tsx
+++ FocalSonic/src-vite/src/app/components/header-song.tsx
@@ -6,18 +6,7 @@ export function HeaderSongInfo() {
     const { currentList, currentSongIndex, currentSong } = usePlayerSonglist();
 
     const isPlaylistEmpty = currentList.length === 0;
-
-    function formatSongCount() {
-        const currentPosition = currentSongIndex + 1;
-        const listLength = currentList.length;
-
-        return `[${currentPosition}/${listLength}]`;
-    }
-
-    function getCurrentSongInfo() {
-        return `${currentSong.artist} - ${currentSong.title}`;
-    }
-
+    
     return (
         <div
             {...igniteViewDragRegion}
@@ -29,11 +18,8 @@ export function HeaderSongInfo() {
                     {...igniteViewDragRegion}
                     className="flex w-full justify-center subpixel-antialiased font-medium text-sm text-muted-foreground"
                 >
-                    <p {...igniteViewDragRegion} className="leading-7 mr-1">
-                        {formatSongCount()}
-                    </p>
                     <p {...igniteViewDragRegion} className="leading-7 truncate">
-                        {getCurrentSongInfo()}
+                        {currentSong.title}
                     </p>
                 </div>
             )}
