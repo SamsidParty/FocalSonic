5ca0feda210a0260738f449d45dcb9c3458111d1
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-12-08 09:26:54 +0300

    Add overflow and height styles to ExtrabarQueue container
    Updated the ExtrabarQueue component's container div to include 'overflow-hidden' and 'h-full' classes, ensuring proper layout and scroll behavior.

--- FocalSonic/src-vite/src/app/components/extrabar/queue.tsx
+++ FocalSonic/src-vite/src/app/components/extrabar/queue.tsx
@@ -3,7 +3,7 @@ import { QueueSongList } from "../queue/song-list";
 
 export default function ExtrabarQueue() {
     return (
-        <div className="p-2">
+        <div className="p-2 overflow-hidden h-full">
             <QueueSongList small={true} />
         </div>
     );
