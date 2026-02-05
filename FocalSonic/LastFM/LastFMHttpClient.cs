94ce4bf754e267887f6d20dae4164d6cf9eab39c
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2026-02-05 19:24:47 +0300

    Last.FM sign in working in C#

--- FocalSonic/LastFM/LastFMHttpClient.cs
+++ FocalSonic/LastFM/LastFMHttpClient.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FocalSonic.LastFM
@@ -15,6 +17,78 @@ namespace FocalSonic.LastFM
         public LastFMHttpClient()
         {
             this.BaseAddress = new Uri("https://ws.audioscrobbler.com/2.0/");
+        }
+
+        // Creates an API signature per https://www.last.fm/api/authspec
+        public static string CreateApiSignature(IDictionary<string, string> parameters, string apiSecret)
+        {
+            // Exclude format param if present when signing
+            var items = parameters
+                .Where(kv => !string.Equals(kv.Key, "format", StringComparison.OrdinalIgnoreCase))
+                .OrderBy(kv => kv.Key, StringComparer.Ordinal)
+                .Select(kv => kv.Key + kv.Value);
+
+            var concatenated = string.Concat(items) + (apiSecret ?? string.Empty);
+
+            using (var md5 = MD5.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(concatenated);
+                var hash = md5.ComputeHash(bytes);
+                var sb = new StringBuilder();
+                foreach (var b in hash) sb.Append(b.ToString("x2"));
+                return sb.ToString();
+            }
+        }
+
+        // Calls auth.getSession with the provided token and returns (sessionKey, username)
+        public async Task<(string sessionKey, string username)> GetSessionAsync(string apiKey, string apiSecret, string token)
+        {
+            if (string.IsNullOrEmpty(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrEmpty(apiSecret)) throw new ArgumentNullException(nameof(apiSecret));
+            if (string.IsNullOrEmpty(token)) throw new ArgumentNullException(nameof(token));
+
+            var parameters = new Dictionary<string, string>
+            {
+                ["method"] = "auth.getSession",
+                ["api_key"] = apiKey,
+                ["token"] = token,
+                ["format"] = "json"
+            };
+
+            var apiSig = CreateApiSignature(parameters, apiSecret);
+            // Add signature to post data
+            var postData = new Dictionary<string, string>(parameters)
+            {
+                ["api_sig"] = apiSig
+            };
+
+            using (var content = new FormUrlEncodedContent(postData))
+            {
+                var resp = await this.PostAsync(string.Empty, content);
+                resp.EnsureSuccessStatusCode();
+                var body = await resp.Content.ReadAsStringAsync();
+
+                // Parse JSON response: { "session": { "name": "...", "key": "..." } }
+                using (var doc = JsonDocument.Parse(body))
+                {
+                    if (doc.RootElement.TryGetProperty("session", out var sessionElem))
+                    {
+                        var name = sessionElem.GetProperty("name").GetString();
+                        var key = sessionElem.GetProperty("key").GetString();
+                        return (key, name);
+                    }
+                    else if (doc.RootElement.TryGetProperty("error", out var errorElem))
+                    {
+                        var code = doc.RootElement.GetProperty("error").GetInt32();
+                        var message = doc.RootElement.GetProperty("message").GetString();
+                        throw new Exception($"Last.fm API error {code}: {message}");
+                    }
+                    else
+                    {
+                        throw new Exception("Unexpected Last.fm response: " + body);
+                    }
+                }
+            }
         }
     }
 }
--- FocalSonic/LastFM/LastFMSignIn.cs
+++ FocalSonic/LastFM/LastFMSignIn.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace FocalSonic.LastFM
 {
@@ -13,6 +14,7 @@ namespace FocalSonic.LastFM
         // This key and callback server belong to SamsidParty
         // Derivatives of FocalSonic not affiliated with SamsidParty should change these
         const string APIKey = "7746ab31f725273e9ae23f25b1e29e12";
+        const string APISecret = "f22c961d06ce812df55182de6bcdd393";
         const string CallbackURL = "https://samsidparty.com/Services/focalsonic/integrations/lastfm.php";
 
         public static bool IsWaitingForToken = false;
@@ -51,22 +53,31 @@ namespace FocalSonic.LastFM
         [Command("recieveLastFMToken")]
         public static async Task RecieveLastFMToken(string token)
         {
+            if (!IsWaitingForToken) return; // Prevents being called multiple times, the token can only be redeemed once
             IsWaitingForToken = false;
 
             // Redeem auth token into session key
             await RedeemAuthToken(token);
 
-            var username = LocalStorage.GetItem("lastfm_username", "default"); ;
-            Console.WriteLine(username);
+            // Callbacks
+            Program.MainWindow?.CallFunction("window._localStorage.hydrate", LocalStorage.GetAllItems("default")); // Reload localStorage
+            Program.MainWindow?.CallFunction("window.reloadLastFMAuthState");
+        }
+
+        [Command("logoutOfLastFM")]
+        public static async Task LogOutOfLastFM()
+        {
+            LocalStorage.RemoveItem("lastfm_session_key", "default");
+            LocalStorage.RemoveItem("lastfm_username", "default");
+
+            Program.MainWindow?.CallFunction("window._localStorage.hydrate", LocalStorage.GetAllItems("default")); // Reload localStorage
+            Program.MainWindow?.CallFunction("window.reloadLastFMAuthState");
         }
 
         public static async Task RedeemAuthToken(string authToken)
         {
-            // TODO: Implement this based on https://www.last.fm/api/show/auth.getSession
-            // Use LastFMHttpClient to make the request, and then store the session key and username
-
-            var sessionKey = "placeholder";
-            var username = "placeholder";
+            // Implemented according to https://www.last.fm/api/show/auth.getSession
+            var (sessionKey, username) = await LastFMHttpClient.Instance.GetSessionAsync(APIKey, APISecret, authToken);
 
             LocalStorage.SetItem("lastfm_session_key", sessionKey, "default");
             LocalStorage.SetItem("lastfm_username", username, "default");
