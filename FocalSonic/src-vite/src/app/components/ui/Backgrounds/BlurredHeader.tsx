ae70184438a0b206f61c896fa91c260faff5673d
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2026-06-21 21:57:26 +0300

    animated blur header

--- FocalSonic/src-vite/src/app/components/ui/Backgrounds/BlurredHeader.tsx
+++ FocalSonic/src-vite/src/app/components/ui/Backgrounds/BlurredHeader.tsx
@@ -2,10 +2,11 @@ export default function BlurredHeader({ bgImage, loaded }: { bgImage: string, lo
     return (
         <div
             style={{
-                backgroundImage: `url(${bgImage})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
+                backgroundImage: `url(${bgImage})`,        
+                backgroundRepeat: "repeat-y",
+                backgroundSize: "100% auto",
                 transition: "opacity 1s ease",
+                animation: "blurredHeaderAnimation 15s ease-in-out infinite",
             }}
             className={`absolute inset-0 w-full h-full blur-3xl ${!loaded ? "opacity-0" : "opacity-70"}`}
         />
